e5e8f120ceee66a9bef6abd7c1b52e5f3c9910d8
Fabert Charles <96333105+helghast098@users.noreply.github.com>
2023-10-28 23:56:46 -0700

    Fixed Bind

--- Bind/bind.c
+++ Bind/bind.c
@@ -13,24 +13,32 @@
 /*Function Definitions*/
 int create_listen_socket(uint16_t port) {
     signal(SIGPIPE, SIG_IGN);
-    if (port == 0) return -1;
+    if (port == 0) { 
+        return -1;
+    }
 
     struct sockaddr_in addr;
     int listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (listenfd < 0) return -2;
+    if (listenfd < 0) {
+        return -2;
+    }
 
     int on = 1;
 
-    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) return -3;
-;
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0) {
+        return -3;
+    }
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htons(INADDR_ANY);
     addr.sin_port = htons(port);
 
-    if (bind(listenfd, (struct sockaddr *) &addr, sizeof(addr) < 0) return -4;
+    if (bind(listenfd, (struct sockaddr *) &addr, sizeof(addr) < 0) {
+        return -4;
+    }
 
-    if (listen(listenfd, 500) < 0) return -4;
-
+    if (listen(listenfd, 500) < 0) {
+        return -4;
+    }
     return listenfd;
 }
